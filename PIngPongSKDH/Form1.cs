a91f3131c411d52433c1b789200177f784215b04
JetRacoon <o-kochergin@bk.ru>
2023-01-18 17:07:28 +0300

    Реализовал алгоритм ПингПонг, который потом будем использовать в СКДХ для проверки связи между стендом и контроллером.

--- PIngPongSKDH/Form1.cs
+++ PIngPongSKDH/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
 
 namespace PIngPongSKDH
 {
@@ -15,6 +17,44 @@ namespace PIngPongSKDH
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FindStand();
+        }
+
+        private bool FindStand()
+        {
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                SerialPort currentPort = new SerialPort(port, 9600);
+                Thread.Sleep(10);
+                currentPort.Open();
+                currentPort.Write("Ping");
+                if (port != "COM1")
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        string answer = currentPort.ReadExisting();
+                        Thread.Sleep(10);
+                        if (answer.Contains("Pong"))
+                        {
+                            textBox1.Text = answer;
+                            Condition.BackColor = Color.Green;
+                            currentPort.Close();
+                            return true;
+                        }
+                        textBox1.Text = answer;
+                    }
+                }
+                currentPort.Close();
+            }
+
+            Condition.BackColor = Color.Red;
+            //textBox1.Text = "Не найдено подходящего устройства";
+            return false;
         }
     }
 }
