539a717c078960256d64af92698cb0bb45c6beab
JetRacoon <o-kochergin@bk.ru>
2023-01-20 13:58:17 +0300

    Изменение интерфейса

--- PIngPongSKDH/Form1.Designer.cs
+++ PIngPongSKDH/Form1.Designer.cs
@@ -34,13 +34,17 @@
             this.Condition = new System.Windows.Forms.Button();
             this.CurrentPort = new System.IO.Ports.SerialPort(this.components);
             this.Load = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Report = new System.Windows.Forms.Button();
+            this.Info = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(10, 30);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(164, 66);
+            this.button1.Size = new System.Drawing.Size(275, 95);
             this.button1.TabIndex = 0;
             this.button1.Text = "Подключиться";
             this.button1.UseVisualStyleBackColor = true;
@@ -48,36 +52,78 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(201, 36);
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(582, 36);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(164, 20);
+            this.textBox1.Size = new System.Drawing.Size(290, 31);
             this.textBox1.TabIndex = 1;
             // 
             // Condition
             // 
             this.Condition.BackColor = System.Drawing.Color.Red;
             this.Condition.Enabled = false;
+            this.Condition.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Condition.ForeColor = System.Drawing.Color.White;
-            this.Condition.Location = new System.Drawing.Point(12, 176);
+            this.Condition.Location = new System.Drawing.Point(10, 440);
             this.Condition.Name = "Condition";
-            this.Condition.Size = new System.Drawing.Size(34, 22);
+            this.Condition.Size = new System.Drawing.Size(275, 70);
             this.Condition.TabIndex = 2;
             this.Condition.UseVisualStyleBackColor = false;
             // 
             // Load
             // 
-            this.Load.Location = new System.Drawing.Point(12, 84);
+            this.Load.Enabled = false;
+            this.Load.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Load.Location = new System.Drawing.Point(10, 160);
             this.Load.Name = "Load";
-            this.Load.Size = new System.Drawing.Size(164, 69);
+            this.Load.Size = new System.Drawing.Size(275, 95);
             this.Load.TabIndex = 3;
             this.Load.Text = "Загрузка";
             this.Load.UseVisualStyleBackColor = true;
+            this.Load.Visible = false;
+            this.Load.Click += new System.EventHandler(this.Load_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(577, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(295, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Серийный номер двигателя:";
+            // 
+            // Report
+            // 
+            this.Report.Enabled = false;
+            this.Report.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Report.Location = new System.Drawing.Point(10, 290);
+            this.Report.Name = "Report";
+            this.Report.Size = new System.Drawing.Size(275, 95);
+            this.Report.TabIndex = 5;
+            this.Report.Text = "Сформировать отчёт";
+            this.Report.UseVisualStyleBackColor = true;
+            this.Report.Visible = false;
+            // 
+            // Info
+            // 
+            this.Info.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Info.Location = new System.Drawing.Point(600, 465);
+            this.Info.Name = "Info";
+            this.Info.Size = new System.Drawing.Size(275, 45);
+            this.Info.TabIndex = 6;
+            this.Info.Text = "Информация";
+            this.Info.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(377, 210);
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(884, 518);
+            this.Controls.Add(this.Info);
+            this.Controls.Add(this.Report);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.Load);
             this.Controls.Add(this.Condition);
             this.Controls.Add(this.textBox1);
@@ -96,6 +142,9 @@
         private System.Windows.Forms.Button Condition;
         private System.IO.Ports.SerialPort CurrentPort;
         private System.Windows.Forms.Button Load;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Report;
+        private System.Windows.Forms.Button Info;
     }
 }
 
--- PIngPongSKDH/Form1.cs
+++ PIngPongSKDH/Form1.cs
@@ -16,6 +16,8 @@ namespace PIngPongSKDH
     {
         List<string> ports = new List<string> { };
         string Port;
+        bool isFound = false;
+        
         public Form1()
         {
             InitializeComponent();
@@ -28,7 +30,6 @@ namespace PIngPongSKDH
                 Port = FindPort();
                 if (Port != null)
                 {
-                    button1.Text = "Отключиться";
                     CurrentPort.PortName = Port;
                     Success();
                     CurrentPort.Open();
@@ -41,7 +42,7 @@ namespace PIngPongSKDH
             }
             else if (button1.Text == "Отключиться")
             {
-                button1.Text = "Подключиться";
+                
                 if (CurrentPort.IsOpen)
                 {
                     CurrentPort.Close();
@@ -53,6 +54,30 @@ namespace PIngPongSKDH
                     ports.Clear();
                     Fail();
                 }
+            }
+        }
+
+        private void Load_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (isFound)
+                {
+                    string data = WaitingAnswer();
+                    if (data != null)
+                    {
+                        textBox1.Text = data;
+                    }
+                    else
+                    {
+                        Fail();
+                    }
+                }
+            }
+            catch
+            {
+                Fail();
+                MessageBox.Show("Проверьте подключение стенда");
             }
         }
 
@@ -70,19 +95,19 @@ namespace PIngPongSKDH
                 foreach (string port in ports)
                 {
                     CurrentPort.PortName = port;
-                    Thread.Sleep(10);
                     CurrentPort.Open();
                     if (port != "COM1")
                     {
                         for (int i = 0; i < 100; i++)
                         {
                             CurrentPort.Write(keyWord);
-
-                            string answer = CurrentPort.ReadExisting();
+                            Thread.Sleep(10);
+                            string answer = CurrentPort.ReadLine();
                             Thread.Sleep(10);
                             if (answer.Contains("Pong"))
                             {
                                 CurrentPort.Close();
+                                isFound = true;
                                 return port;
                             }
                         }
@@ -92,13 +117,18 @@ namespace PIngPongSKDH
             }
             catch
             {
+                isFound = false;
                 MessageBox.Show("Проверьте подключение стенда");
             }
+            isFound = false;
             return null;
         }
 
         private void Success()
         {
+            button1.Text = "Отключиться";
+            Load.Visible = true;
+            Load.Enabled = true;
             Condition.Text = "Успешное подключение";
             Condition.BackColor = Color.Green;
             Condition.ForeColor = Color.Black;
@@ -106,9 +136,29 @@ namespace PIngPongSKDH
 
         private void Fail()
         {
+            button1.Text = "Подключиться";
+            Load.Visible = false;
+            Load.Enabled = false;
             Condition.Text = "Нет подключения";
             Condition.BackColor = Color.Red;
             Condition.ForeColor = Color.Black;
+        }
+
+        private string WaitingAnswer()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                CurrentPort.Write("Load");
+                Thread.Sleep(10);
+                string answer = CurrentPort.ReadLine();
+                string message = null;
+                if (answer.StartsWith("[") && answer.EndsWith("\r"))
+                {
+                    message = answer;
+                    return answer;
+                }
+            }
+            return null;
         }
     }
 }
